ee5201a1f1712faadc7b12f95a02ce33b0a15bea
americandev <kubadaev48@gmail.com>
2023-07-01 14:13:34 +0600

    AmanAsanbekov DZ4

---
+++ app.js
@@ -0,0 +1,21 @@
+const btn = document.querySelector('button')
+
+btn.addEventListener('click', ()=> {
+    const req = new XMLHttpRequest()
+    req.open("GET", "people.json")
+    req.setRequestHeader("Content-type", "application/json")
+    req.send()
+    req.addEventListener("load", ()=> {
+        const date = JSON.parse(req.response)
+        date.forEach(people => {
+            const names = document.createElement('div')
+            names.innerText = 'name: ' + people.name
+            document.body.append(names)
+            const age = document.createElement('div')
+            age.innerText = 'age: ' + people.age
+            document.body.append(age)
+            const wrapper = document.querySelector('.wrapper')
+            wrapper.append(names, age)
+        })
+    })
+})
